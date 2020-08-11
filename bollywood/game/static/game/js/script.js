ebbb8857eb46699e88349b54d9650e9c059e426c
Kashish-max <kashish24102001@gmail.com>
2020-08-11 20:44:48 +0530

    Updated required modifications

--- bollywood/game/static/game/js/script.js
+++ bollywood/game/static/game/js/script.js
@@ -1,25 +1,46 @@
-var vowels = [ 'a', 'e', 'i', 'o', 'u','A','E','I','O','U'];
+var vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
 
 var movieFromBackend = "Avengers Endgame";
 
 var movieName = "";
 
-for(var j = 0 ; j < movieFromBackend.length ; j++)
-{
-    if(vowels.indexOf(movieFromBackend[j]) === -1 && movieFromBackend[j] !== ' ')
-    {
-        movieName += "_";
+String.prototype.replaceAt = function (index, replacement) {
+  return (
+    this.substr(0, index) +
+    replacement +
+    this.substr(index + replacement.length)
+  );
+};
+
+function checkLetter() {
+  for (var j = 0; j < movieFromBackend.length; j++) {
+    if (
+      vowels.indexOf(movieFromBackend[j]) === -1 &&
+      movieFromBackend[j] !== " "
+    ) {
+      movieName += "_";
+    } else if (movieFromBackend[j] === " ") {
+      movieName += " ";
+    } else {
+      movieName += movieFromBackend[j];
     }
-    else if(movieFromBackend[j] === ' ')
-    {
-        movieName +=' ';
-    }
-    else
-    {
-        movieName += movieFromBackend[j];
-    }
+  }
 }
 
-document.getElementById("displayQuestion").innerHTML=movieName;
+checkLetter();
+document.getElementById("displayQuestion").innerHTML = movieName;
 
-console.log(movieName);
+var testMovie;
+function testFunction(el) {
+  if (movieFromBackend.indexOf(el) !== -1) {
+    for (var i = 0; i < movieFromBackend.length; i++) {
+      if (movieFromBackend[i] === el) {
+        testMovie = movieName.replaceAt(i, el);
+        movieName = testMovie;
+      }
+    }
+  } else {
+    //Fade Bollywood
+  }
+  document.getElementById("displayQuestion").innerHTML = movieName;
+}
