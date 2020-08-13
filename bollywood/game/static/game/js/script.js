78922856cb5227e1415337101366a2e341ca8665
Kashish-max <kashish24102001@gmail.com>
2020-08-13 14:20:02 +0530

    Updated play modes

--- bollywood/game/static/game/js/script.js
+++ bollywood/game/static/game/js/script.js
@@ -1,5 +1,9 @@
 var vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
 
+upper_layer = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p"];
+middle_layer = ["a", "s", "d", "f", "g", "h", "j", "k", "l"];
+bottom_layer = ["z", "x", "c", "v", "b", "n", "m"];
+
 var movieFromBackend = "Avengers Endgame";
 
 var movieName = "";
@@ -11,6 +15,21 @@ String.prototype.replaceAt = function (index, replacement) {
     this.substr(index + replacement.length)
   );
 };
+
+document.getElementById("displayGame").style.display = "none";
+document.getElementById("gameMode").style.display = "none";
+
+function chooseMode() {
+  document.getElementById("displayPlay").style.display = "none";
+  document.getElementById("gameMode").style.display = "block";
+}
+
+function playGame(el) {
+  document.getElementById("gameMode").style.display = "none";
+  document.getElementById("displayGame").style.display = "block";
+  checkLetter();
+  document.getElementById("displayQuestion").innerHTML = movieName;
+}
 
 function checkLetter() {
   for (var j = 0; j < movieFromBackend.length; j++) {
@@ -26,9 +45,6 @@ function checkLetter() {
     }
   }
 }
-
-checkLetter();
-document.getElementById("displayQuestion").innerHTML = movieName;
 
 var testMovie;
 function testFunction(el) {
